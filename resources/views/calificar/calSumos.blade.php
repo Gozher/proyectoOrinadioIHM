7ea12356a5689373f18960ce042bfcb6ada53014
LevyMerino <merino.levy@gmail.com>
2019-06-06 20:06:11 -0500

    Cambios de blade calificar

--- resources/views/calificar/calSumos.blade.php
+++ resources/views/calificar/calSumos.blade.php
@@ -8,7 +8,7 @@
 
     <div class="pull-center">
 
-        <h2>Calificar Sumos</h2>
+       <center> <h2>Calificar Sumos</h2> </center>>
 
     </div>
 
@@ -58,25 +58,36 @@
 
     <td>
 
-        <form action="{{ route('Sumos.destroy',$sumo->id) }}" method="POST">
+       < <form action="{{ route('Sumos.destroy',$sumo->id) }}" method="POST"> 
 
+          <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" href="{{route('Sumos.edit',$Sumos->id)}}">Calffff</a>
 
+            <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-
-
-            <a class="btn btn-primary" href="{{ route('Sumos.edit',$sumo->id) }}">Calificar</a>
-
-
-
-            @csrf
-
-            @method('DELETE')
 
 
 
            
-        </form>
+        </form> 
 
     </td>
 
